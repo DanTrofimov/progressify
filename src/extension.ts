14faf4026cf39f68f6d56189415b1144d46f5f55
Dan Trofimov <daniil.trofimov@sbermarket.ru>
2023-05-30 08:45:08 +0300

    add preview service

--- src/extension.ts
+++ src/extension.ts
@@ -1,34 +1,31 @@
 import * as vscode from 'vscode';
+import { ManifestPreviewService } from './services/ManifestPreviewService';
 
 export function activate(context: vscode.ExtensionContext) {
-	context.subscriptions.push(
-		vscode.commands.registerCommand('progressify.startPreview', () => {
-		  // Create and show a new webview
-		  const panel = vscode.window.createWebviewPanel(
-			'progressify', // Identifies the type of the webview. Used internally
-			'Manifest Preview', // Title of the panel displayed to the user
-			vscode.ViewColumn.Two, // Editor column to show the new webview panel in.
-			{} // Webview options. More on these later.
-		  );
-		   // And set its HTML content
-		   panel.webview.html = getWebviewContent();
-		})
+	const contentProvider = new ManifestPreviewService(context);
+
+	function openPreviewToTheSide(uri: vscode.Uri) {
+		let resource = uri;
+		if (!(resource instanceof vscode.Uri)) {
+		  if (vscode.window.activeTextEditor) {
+			// we are relaxed and don't check for markdown files
+			resource = vscode.window.activeTextEditor.document.uri;
+		  }
+		}
+		contentProvider.initPreview(resource, {
+		  viewColumn: vscode.ViewColumn.Two,
+		  preserveFocus: true,
+		});
+	  }
+
+	  context.subscriptions.push(
+		vscode.commands.registerCommand(
+		  "progressify.sidePreview",
+		  openPreviewToTheSide,
+		),
 	  );
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
-
-function getWebviewContent() {
-	return `<!DOCTYPE html>
-  <html lang="en">
-  <head>
-	  <meta charset="UTF-8">
-	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	  <title>Cat Coding</title>
-  </head>
-  <body>
-	  <img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" width="300" />
-  </body>
-  </html>`;
-  }
+export function deactivate() {
+}
