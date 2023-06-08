095d01b4e7881240cb85ba33ce7e734b1f17226c
Dan Trofimov <daniil.trofimov@sbermarket.ru>
2023-06-09 02:31:57 +0300

    add manifest parse

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -12,7 +12,53 @@ function eventHandler(event) {
     }
 };
 
+// check required fields
+function validatePayload(payload) {
+    if (!payload.name) {return false;}
+    if (!payload.icons) {return false;}
+    if (!payload.start_url) {return false;};
+    if (!payload.display) {return false;};
+    return true;
+}
+
 function onUpdate(payload) {
-    const contentRoot = document.querySelector(".preview-app-content");
-    contentRoot.textContent = payload;
+
+    const parsedPayload = JSON.parse(payload);
+
+    if (!validatePayload(parsedPayload)) {
+        console.log('Some required fields are expected');
+        return;
+    }
+
+    const { theme_color, background_color, name, icons } = parsedPayload;
+
+    updateHeader(theme_color);
+    updatePreviewContainerBackground(background_color);
+    updateName(name);
+    updateIcon(icons);
+}
+
+function updateHeader(themeColor) {
+
+    const headerNode = document.querySelector(".preview-header");
+
+    headerNode.style.backgroundColor = themeColor;
+}
+
+function updateName(name) {
+    const nameNode =  document.querySelector(".container__name");
+
+    nameNode.textContent = name;
+}
+
+function updateIcon(icons) {
+    const iconNode = document.querySelector(".container__logo");
+
+    iconNode.src = icons[0].src;
+}
+
+function updatePreviewContainerBackground(backgroundColor) {
+    const previewContainerNode = document.querySelector('.preview-container');
+
+    previewContainerNode.style.backgroundColor = backgroundColor;
 }
--- src/utils/previewTemplate.ts
+++ src/utils/previewTemplate.ts
@@ -5,23 +5,26 @@ export const previewTemplate = (stylesPath: vscode.Uri, scriptsPath: vscode.Uri,
         <!DOCTYPE html>
         <html lang="en">
         <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <link rel="stylesheet" type="text/css" href="${stylesPath}">
-            <title>Cat Coding</title>
+            <meta charset="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <link rel="stylesheet" type="text/css" href="${stylesPath}" />
+            <title>Manifest Preview</title>
         </head>
-
-            <body>
-                <div class="preview-container">
-                    <header class="preview-header">
-                        
-                    </header>
-                    <pre class="preview-app-content">
-                        
-                    </pre>
-                </div>
-                <script type="text/javascript" src="${scriptsPath}"></script>
-            </body>
-        </html>
-`;
+        
+        <body>
+            <div class="preview-container">
+                <header>
+                    <div class="preview-header"></div>
+                </header>
+                <main class="preview-app-content">
+                    <div class="preview-app-content__container">
+                        <img class="container__logo"/>
+                        <h1 class="container__name"></h1>
+                    </div>
+                </main>
+            </div>
+            <script type="text/javascript" src="${scriptsPath}"></script>
+        </body>
+        </html>    
+    `;
 };
