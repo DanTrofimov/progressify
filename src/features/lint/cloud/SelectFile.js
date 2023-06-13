656e4e5e03583266e97b3b800c41ad0da68b1ec9
Dan Trofimov <daniil.trofimov@sbermarket.ru>
2023-06-13 19:44:14 +0300

    setup cloud function

--- src/features/lint/cloud/SelectFile.js
+++ src/features/lint/cloud/SelectFile.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
+const path = require('path');
+const fetch = require('node-fetch-commonjs');
 
 const parser = require('@babel/parser');
 const generate = require("@babel/generator").default;
 
-const path = require('path');
 const fileDir = path.join(__dirname, '../plugin/example');
 const filePath = path.join(fileDir, 'sw.js');
 const outputFilePath = path.join(fileDir, 'ast.json');
@@ -20,7 +21,18 @@ const ast = parser.parse(code, {
 const astJson = JSON.stringify(ast);
 fs.writeFileSync(outputFilePath, astJson);
 
-const generatedFile = generate(ast);
+const API_KEY = 'Api-Key';
+const CLOUD_FUNC_ENDPOINT = 'https://functions.yandexcloud.net/d4e5igjblkjsmcp0u277';   
 
-// result of generation from ast
-fs.writeFileSync(codegenFilePath, generatedFile.code.toString());
+const response = fetch(CLOUD_FUNC_ENDPOINT, {
+    method: 'POST', 
+    body: astJson,
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': API_KEY,
+    },
+    isBase64Encoded: false
+}).then((response) => response.json())
+    .then((data) => {   
+    console.log(data);
+});
